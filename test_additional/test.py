9647a7bf107953f17df62c7bc550841bcef1a152
Catherine <chernogor_k@mail.ru>
2026-03-04 14:32:43 +0800

    Show all test cases not passed

--- test_additional/test.py
+++ test_additional/test.py
@@ -5,5 +5,10 @@ from math_demo import add
 def test_addition():
     assert add(2,2)==4
     print("test Addition passed")
+
+def test_bug_additional():
+    assert add_with_bug(2,2)== 4 , "function didn't returned"
+    print('test Addition passerd')
 if __name__ == "__main__":
     test()
+    
