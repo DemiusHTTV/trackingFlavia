687df5d1a02ec5f88275c9fd8642b786175a2ed8
Sasha <sasabuzinaev@gmail.com>
2026-03-30 21:42:38 +0800

    Delete: replace library to upper node directory

--- mtackerJFlavia/mtracker_jflavia/__init__.py
+++
@@ -1,2 +0,0 @@
-from .tracker import execute_and_get_memory_usage
-from .josephus import IosifeFlavia
--- mtackerJFlavia/mtracker_jflavia/josephus.py
+++
@@ -1,8 +0,0 @@
-def IosifeFlavia(arr,n,k):
-    if len(arr) == 1:
-        return arr[0]
-    else:
-        index = (k-1) % len(arr)
-        arr.pop(index)
-        return IosifeFlavia(arr,n,k)
-    
