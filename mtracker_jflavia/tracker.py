4ad43efddd6e1233963f67e41809de8a91e6e104
Sasha <sasabuzinaev@gmail.com>
2026-03-30 22:15:23 +0800

    Refactore: improved the function in the decorator

--- mtracker_jflavia/tracker.py
+++ mtracker_jflavia/tracker.py
@@ -1,12 +1,21 @@
 import sys
 import tracemalloc
+from functools import wraps
 
-def execute_and_get_memory_usage(function, n):
-    tracemalloc.start()
-    before = tracemalloc.get_traced_memory()
-    data = function(n)
-    after = tracemalloc.get_traced_memory()
-    tracemalloc.stop()
-    print(f"According to tracemalloc: {after[1] - before[1]}")
-    print(f"According to sys: {sys.getsizeof(data)}")
-    return data 
+def execute_and_get_memory_usage(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+        before = tracemalloc.get_traced_memory()
+
+        data = func(*args, **kwargs)
+
+        after = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+
+        print(f"According to tracemalloc: {after[1] - before[1]}")
+        print(f"According to sys: {sys.getsizeof(data)}")
+
+        return data
+
+    return wrapper
